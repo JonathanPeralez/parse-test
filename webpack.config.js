692efd11f0c0b3e4a59b42258ea48604604440ac
Jonathan Peralez <jperalez@gmail.com>
2019-05-18 16:14:49 -0700

    Project setup

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  // change entry file to the path/file of your React app
+  entry: './client/index.js',
+  mode: 'development',
+  devServer: {
+    publicPath: '/build/',
+    proxy: {
+      '/api': 'http://localhost:3000',
+    },
+    historyApiFallback: true,
+    // contentBase: './'
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+      }
+    }]
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  }
+};
